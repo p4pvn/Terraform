32e631888395730a76505092a98a1c97ea762dae
Pawan <91900183+p4pvn@users.noreply.github.com>
2025-08-20 20:51:28 +0530

    Update output.tf

--- terraform-modules/networking/vpc/module/output.tf
+++ terraform-modules/networking/vpc/module/output.tf
@@ -1,1 +1,15 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "fsx-vpc"
+  }
+}
 
+resource "aws_subnet" "public" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "fsx-subnet"
+  }
+}
