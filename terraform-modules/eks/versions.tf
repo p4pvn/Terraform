0dce01426fbed12b4665af42ebe9884d7f05779b
Pawan <91900183+p4pvn@users.noreply.github.com>
2025-08-20 21:08:01 +0530

    Update versions.tf

--- terraform-modules/eks/versions.tf
+++ terraform-modules/eks/versions.tf
@@ -1,10 +1,15 @@
 terraform {
-  required_version = ">= 1.5.0"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.1"
+      version = ">= 5.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.15"
+    }
+    tls = {
+      source = "hashicorp/tls"
+      version = ">= 4.0"
     }
   }
-}
