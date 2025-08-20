52ac0a458ee5d8f0e6a668b627e96579d44311f8
Pawan <91900183+p4pvn@users.noreply.github.com>
2025-08-20 21:09:57 +0530

    Update main.tf

--- terraform-modules/eks/main.tf
+++ terraform-modules/eks/main.tf
@@ -1,1 +1,16 @@
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "${var.cluster_name}-cluster-role"
+
+  # The cluster will assume this role that is alreay created by aws readily.
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
 
